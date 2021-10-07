71626e03efd6f63e53abe9218a1218a7046127c9
rayvaldez <ray_valdez@hotmail.co.uk>
2021-10-07 16:00:03 +0100

    Add index/show/create methods to Player controller

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -1,2 +1,30 @@
 class Api::V1::PlayersController < ApplicationController
+
+  def index
+    players = Player.all
+
+    render json: players, status:200
+  end
+
+  def show
+    player = Player.find(params[:id])
+
+    render json: player, status:200
+  end
+
+  def create
+    player = Player.create(player_params)
+
+    if player.save
+      render json: player, status:200
+    else
+      render json: player, status:400
+    end
+  end
+
+  private
+
+  def player_params
+    params.require(:player).permit(:name, :team, :image, :position)  
+  end
 end
