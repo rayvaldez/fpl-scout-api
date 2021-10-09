d947d12f0dee146cb1d1cd85d288fe9ae38aa6b9
rayvaldez <ray_valdez@hotmail.co.uk>
2021-10-09 20:13:52 +0100

    Add methods to Comments controller

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,28 @@
 class CommentsController < ApplicationController
+
+  def index
+    player = Player.find(params[:player_id])
+    comments = player.comments
+
+    render json: comments, status:200
+  end
+
+  def create
+    player = Player.find(params[:player_id])
+    comment = player.comments.create(comment_params)
+
+    render json: comment, status:200
+  end
+
+  def show
+    comment = Comment.find(params[:id])
+
+    render json: comment, status:200
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:player_id, :text)
+  end
 end
