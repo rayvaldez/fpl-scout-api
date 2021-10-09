fc3229c9ab93f8afa9d076e0564caeb8a721ebfc
rayvaldez <ray_valdez@hotmail.co.uk>
2021-10-09 20:57:17 +0100

    Add validations to all models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :player
+
+  validates :text, presence: true
+  validates :text, length: { minimum: 2 }
+  validates :gameweek, inclusion: { :in => 0..38 }
 end
--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,3 +1,6 @@
 class Reply < ApplicationRecord
   belongs_to :comment
+
+  validates :text, presence: true
+  validates :text, length: { minimum: 2 }
 end
