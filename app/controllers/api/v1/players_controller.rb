bef8007d41458d407f8f91f202ef09f3ae8a10cc
rayvaldez <ray_valdez@hotmail.co.uk>
2021-10-17 08:11:44 +0100

    Add fetch method to call api and create Player objects

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::PlayersController < ApplicationController
+  before_action :fetch_players, only: [:index]
+
+  require 'rest-client'
+  require 'json'
 
   def index
     players = Player.all
@@ -24,7 +28,18 @@ class Api::V1::PlayersController < ApplicationController
 
   private
 
+  def fetch_players
+    url = 'https://fantasy.premierleague.com/api/bootstrap-static/'
+    data = RestClient.get(url)
+    parsed = JSON.parse(data)
+    p = parsed["elements"]
+    player_array = p.map { |a| [a['first_name'] + " " + a['second_name'], a['now_cost'], a['team']]}
+    players = player_array.map { |a| Player.create(name: a[0], price:a[1], team:a[2])}
+
+    puts players
+  end
+
   def player_params
-    params.require(:player).permit(:name, :team, :position, :price)  
+    params.require(:player).permit(:name, :team, :position, :price)
   end
 end
