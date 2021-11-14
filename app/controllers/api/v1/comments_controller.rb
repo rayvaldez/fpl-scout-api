9e337f07e088accadc0827ce8ea5a5dd485ac54b
rayvaldez <ray_valdez@hotmail.co.uk>
2021-11-14 13:36:30 +0000

    Remove player variable in create method

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,15 +1,13 @@
 class Api::V1::CommentsController < ApplicationController
 
   def index
-    player = Player.find(params[:player_id])
-    comments = player.comments
+    comments = Comment.all
 
     render json: comments, status:200
   end
 
   def create
-    player = Player.find(params[:player_id])
-    comment = player.comments.create(comment_params)
+    comment = Comment.create(comment_params)
 
     render json: comment, status:200
   end
@@ -23,6 +21,6 @@ class Api::V1::CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:player_id, :text)
+    params.require(:comment).permit(:player_id, :text, :gameweek)
   end
 end
