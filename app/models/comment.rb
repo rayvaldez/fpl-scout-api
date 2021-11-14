3baf06a859315863ca76861be21ae96405152e6b
rayvaldez <ray_valdez@hotmail.co.uk>
2021-11-14 13:33:37 +0000

    Add authentication for player_id presence

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,5 @@ class Comment < ApplicationRecord
   validates :text, presence: true
   validates :text, length: { minimum: 2 }
   validates :gameweek, inclusion: { :in => 0..38 }
+  validates :player_id, presence: true
 end
